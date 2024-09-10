1ecb32d692df0dd39dfd8178c73efcd5e535fc14
gustavorisio <spgustavorisio@gmail.com>
2024-09-09 23:43:33 -0300

    TypeORM e integracao com o banco de dados

--- API/src/service/user/CreateUserService.ts
+++ API/src/service/user/CreateUserService.ts
@@ -1,5 +1,7 @@
 import { IUserRequest } from "../../interface/IUserInterface";
-
+import { hash } from "bcryptjs";
+import { UsersRepositories } from "../../repositories/userRepositories";
+import { getCustomRepository } from "typeorm";
 class CreateUserService {
     async execute({ name, email, admin = false, password }: IUserRequest) {
         if (!email) {
@@ -8,7 +10,18 @@ class CreateUserService {
         if (!password) {
             throw new Error("Password Incorrect")
         }
-        return { message: "User incluido com sucesso" };
+        const passwordHash = await hash(password, 8);
+
+        const usersRepository = getCustomRepository(UsersRepositories)
+        const user = usersRepository.create(
+            {
+                name,
+                email,
+                admin,
+                password: passwordHash,
+            });
+            await usersRepository.save(user);
+            return user;
     }
 }
 export { CreateUserService };
